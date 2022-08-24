e65c53da10267223cb163c43b840591d5c8b2c02
yogeshymca23 <yogeshymca23@gmail.com>
2022-08-24 23:33:39 +0530

    v2

--- src/components/post.js
+++ src/components/post.js
@@ -37,7 +37,7 @@ function Post() {
         const img = await res.json();
         // console.log(img);
         const link = img.secure_url;  // ye link bhi aaega
-        
+
         // console.log(img.secure_url);
         // setlink(img.secure_url);
 
@@ -52,8 +52,8 @@ function Post() {
                 });
     
     
-            // window.location.reload(false);
-            // alert("Paper Uploaded Succesfully");
+            window.location.reload(false);
+            alert("Paper Uploaded Succesfully");
         
 
         // uploadFile(url, file);
