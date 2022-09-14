c5b33c202d796c5c6a594576c532171e7471becd
yogeshymca23 <yogeshymca23@gmail.com>
2022-09-15 00:24:53 +0530

    al cards

--- src/components/home.js
+++ src/components/home.js
@@ -20,6 +20,14 @@ const Home = () => {
 
     const [Event, setEvent] = useState([])
 
+    const [AllEvent, setAllEvent] = useState([])
+    Axios.get('https://examsopedia.herokuapp.com/get_post').then(res => {
+                setAllEvent(res.data)
+                console.log(res.data)
+            }).catch(function (error) {
+                console.log(error);
+            });
+
     const [keyword, setKeyword] = useState("");
 
     // const onKeyPressHandler = e => {
@@ -373,14 +381,6 @@ const Home = () => {
                     </div>
                 </section>
 
-
-
-                {/* <!-- home section ends --> */}
-
-
-
-                {/* <!-- about section starts  --> */}
-
                 <section id="about">
 
                     <div class="search-box d-md-flex">
@@ -393,7 +393,160 @@ const Home = () => {
                 {isLoading ? <LoadingSpinner /> : null}
 
 
+
+                {/* <!-- home section ends --> */}
+
+                <section id="results" >
+                    {
+
+
+
+                        AllEvent.map((val, key) => {
+
+                            // image url nikalne ka trika
+
+
+                            // console.log(val.auther)
+                            // let base64String;
+                            // let imgaddress = "#";
+
+                            // if (val.img) {
+                            //     base64String = btoa(
+                            //         String.fromCharCode(...new Uint8Array(val.img.data.data))
+                            //     );
+
+                            //     imgaddress = `data:image/png;base64,${base64String}`;
+                            // }
+
+
+
+                            // console.log(val.link);
+
+
+                            return (
+
+                                <div>
+
+                                    <div class="courses-container">
+
+                                        <div class="course row">
+                                            <div class="col-12 col-md-3 py-4 d-flex flex-column align-items-center justify-content-center course-preview">
+                                                <h6>{val.college}</h6>
+                                                <h2>{val.branch}</h2>
+                                                <a >Post by :- {val.auther} ({val.batch})</a>
+                                            </div>
+
+                                            <div class="col-12 col-md-9 course-info px-5">
+                                                <div class="row pt-3">
+                                                    <div class="col-6 ">
+                                                        <h6>{val.year} Year</h6>
+                                                    </div>
+                                                    <div class="col-6 col-md-6  progress-container">
+                                                        <div class="progress"></div>
+                                                        <span class="progress-text">
+                                                            {val.rating}/5 Ratings
+                                                        </span>
+                                                    </div>
+                                                </div>
+
+                                                {/* <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop --> */}
+                                                <div class="row offset-5">
+                                                    <div class="col-12 col-md-12 py-2 ">
+                                                        <h2 id="title">{val.title} </h2>
+                                                    </div>
+
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-12 align-items-start">
+                                                        <h3 id="title">By :- {val.teacher} </h3>
+                                                    </div>
+
+                                                </div>
+
+                                                {/* <!-- Columns are always 50% wide, on mobile and desktop --> */}
+                                                <div class="row">
+                                                    <div class=" col py-3">
+                                                        <button class="btn-card" ><a style={{ color: "white" }} href={val.link}>Download Paper</a></button>
+                                                        {/* ab popup aaega  */}
+                                                        <div class="modal fade"
+                                                            id="exampleModal"
+                                                            tabindex="-1"
+                                                            role="dialog"
+                                                            aria-labelledby="exampleModalLabel"
+                                                            aria-hidden="true">
+
+                                                            <div class="modal-dialog" role="document">
+                                                                <div class="modal-content">
+
+                                                                    {/* <!-- Add image inside the body of modal --> */}
+                                                                    <div class="modal-body">
+                                                                        <img id="image" src={val.link} width="100%"
+                                                                            alt="No Data Found" />
+                                                                    </div>
+
+                                                                    <div class="modal-footer">
+                                                                        <button type="button"
+                                                                            class="btn-card"
+                                                                            data-dismiss="modal">
+                                                                            Close
+                                                                        </button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+
+
+                                            {/* <div class="row course-info">
+                                                <div class="col progress-container">
+                                                    <div class="progress"></div>
+                                                    <span class="progress-text">
+                                                        {val.rating}/5 Ratings
+                                                    </span>
+                                                    <h6 class="col-4">{val.year} Year</h6>
+                                                </div>
+
+                                                <h2 class="col" id="title">{val.title} </h2>
+
+                                                <h2 class="col"> (by {val.teacher})</h2>
+                                                <button class="row btn-card" data-toggle="modal"
+                                                    data-target="#exampleModal">View Paper</button>
+
+                                                
+                                            </div> */}
+                                        </div>
+                                    </div>
+
+                                    {/* <button class="floating-btn">
+                                        <a href='/post' class="floating-btn" >Upload New Paper</a>
+                                    </button> */}
+
+
+
+                                </div>
+                            )
+                        })
+                    }
+                </section>
+
+
+
+                {/* <!-- about section starts  --> */}
+
+                
+
+
                 {/* <!-- about section ends --> */}
+
+
+
+
+                
 
 
 
@@ -410,28 +563,11 @@ const Home = () => {
 
 
 
-                <div class="courses-container">
+                {/* <div class="courses-container">
                     <div class="course">
-                        {/* <div class="course-preview">
-                            <h6>---</h6>
-                            <h2>---</h2>
-                            <a >Post by :-Nonw</a>
-                        </div> */}
+                        
 
                         <div class="course-info">
-                            {/* <div class="row ">
-                                <div class="col-6 ">
-                                    <h6>XXXX Year</h6>
-                                </div>
-                                <div class="col-6 col-md-6 col-sm-12 col-xs-12 progress-container">
-                                    <div class="progress"></div>
-                                    <span class="progress-text">
-                                        -/5 Ratings
-                                    </span>
-                                </div>
-                            </div> */}
-
-                            {/* <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop --> */}
                             <div class="row offset-5">
                                 <div class="col-12 col-md-12 py-2 ">
                                     <h2 id="title">No Results till now </h2>
@@ -445,13 +581,11 @@ const Home = () => {
 
                             </div>
 
-                            {/* <!-- Columns are always 50% wide, on mobile and desktop --> */}
-
 
                         </div>
 
                     </div>
-                </div>
+                </div> */}
             </div >
 
 
